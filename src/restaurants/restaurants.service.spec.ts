617a265e363a515eca037cb302a7116f19efcf84
Bikesh <bikesh17stha@gmail.com>
2023-02-07 11:45:17 +0545

    Unit testing for Restaurant service

--- src/restaurants/restaurants.service.spec.ts
+++ src/restaurants/restaurants.service.spec.ts
@@ -4,8 +4,22 @@ import { model, Model } from 'mongoose'
 import { RestaurantsService } from './restaurants.service'
 import { Restaurant } from './schemas/restaurants.schema'
 
-
-const mockRestaurantService = {}
+const mockRestaurant = {
+    "_id": "63db7ad0d47e973533dbc4cd",
+    "name": "Matidevi Fast Foood Restaurant",
+    "description": "Made from boban",
+    "email": "umb@umb.com",
+    "phoneNo": 9788246116,
+    "address": "Kakni Fish Farm",
+    "category": "Fast Food",
+    "images": [],
+    "menu": [],
+    "createdAt": "2023-02-02T08:56:48.255Z",
+    "updatedAt": "2023-02-02T08:56:48.255Z"
+}
+const mockRestaurantService = {
+    find: jest.fn(),
+}
 describe('Restaurant Service', () => {
 
 
@@ -27,5 +41,18 @@ describe('Restaurant Service', () => {
     })
     it('should be defined', () => {
         expect(service).toBeDefined();
+    })
+
+    describe('FindAll', () => {
+        it('should get all the restaurants', async () => {
+            jest.spyOn(model, 'find').mockImplementationOnce(() => ({
+                limit: () => ({
+                    skip: jest.fn().mockResolvedValue([mockRestaurant]),
+                }),
+            } as any));
+
+            const restaurants = await service.findAll({ keyword: 'restaurant' });
+            expect(restaurants).toEqual([mockRestaurant])
+        })
     })
 })
