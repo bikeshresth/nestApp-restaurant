c4b5d00c2ebcadde6d49074c9f718dd34fb724fd
Bikesh <bikesh17stha@gmail.com>
2023-02-08 14:09:46 +0545

    Changed the path of the system 5

--- src/meal/meal.controller.ts
+++ src/meal/meal.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Delete, ForbiddenException, Get, Param, Post, Put } from '@nestjs/common';
-import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
-import { User } from 'src/auth/schemas/user.schema';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { User } from '../auth/schemas/user.schema';
 import { CreateMealDto } from './dto/create-meal.dto';
 import { UpdateMealDto } from './dto/update-meal.dto';
 import { MealService } from './meal.service';
